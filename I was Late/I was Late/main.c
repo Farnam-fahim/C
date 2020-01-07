aed0f0e5af8afffd24bca2afe30c29e572e7d83b
Farnam-fahim <56405736+Farnam-fahim@users.noreply.github.com>
2020-01-07 12:24:36 -0800

    Add files via upload

--- I was Late/I was Late/main.c
+++ I was Late/I was Late/main.c
@@ -2,72 +2,73 @@
 #include <stdlib.h>
 
 
-//int sum(int a, int b);
-//
-//int main() {
-//
-//	int x = 0;
-//	while (x < 11)
-//	{
-//		printf("%d", x);
-//		x += 1;
-//		}
-//
-//	printf("__\n\n");
-//	do
-//
-//	{
-//		x += 1;
-//		if (x == 5) break;
-//
-//		printf("%d", x);
-//
-//	} while (x < 11);
-//
-//	int a;
-//	int b;
-//	printf("insert first number\n");
-//	scanf("%d", &a);
-//	printf("insert second number\n");
-//	scanf("%d", &b);
-//
-//
-//	sum(a, b);
-//	
-//
-//
-//	system("pause");
-//
-//}
-//
-//int sum(int a, int b) {
-//	int i;
-//	int multi = 0;
-//	for (i = 0; i <= 100; i++)
-//	{
-//		multi += i;
-//	}
-//	printf("%d\n", multi);
-//	return multi;
-//	
-//}
+int MySum(int a, int b);
 
 int main() {
 
-	printf("1.Book seats\n");
+	/*int x = 0;
+	while (x < 11)
+	{
+		printf("%d", x);
+		x += 1;
+		}
 
-	printf("2.buy seats\n");
+	printf("__\n\n");
+	do
 
-	printf("3.buy seats\n");
+	{
+		x += 1;
+		if (x == 5) break;
 
-	printf("5.buy seats\n");
+		printf("%d", x);
 
-	printf("6.buy seats\n");
+	} while (x < 11);*/
 
-	printf("7.buy seats\n");
+	int a;
+	int b;
+	printf("insert first number\n");
+	scanf("%d", &a);
+	printf("insert second number\n");
+	scanf("%d", &b);
 
-	printf("8.buy seats\n");
+
+	MySum(a, b);
+	
 
 
-		system("pause");
+	system("pause");
+
 }
+
+int MySum(int a, int b) {
+	
+	int multi = 0;
+	
+	for (a; a<(b+1); a++)
+	{
+		multi = multi + a;
+	}
+	printf("%d\n", multi);
+	return multi;
+	
+}
+
+//int main() {
+//
+//	printf("1.Book seats\n");
+//
+//	printf("2.buy seats\n");
+//
+//	printf("3.buy seats\n");
+//
+//	printf("5.buy seats\n");
+//
+//	printf("6.buy seats\n");
+//
+//	printf("7.buy seats\n");
+//
+//	printf("8.buy seats\n");
+//
+//
+//		system("pause");
+//}
