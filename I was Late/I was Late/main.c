17ba4ac0d3513f1da2b5863e7c789f8a777b67fa
Farnam-fahim <56405736+Farnam-fahim@users.noreply.github.com>
2020-01-07 11:49:13 -0800

    Add files via upload

---
+++ I was Late/I was Late/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//int sum(int a, int b);
+//
+//int main() {
+//
+//	int x = 0;
+//	while (x < 11)
+//	{
+//		printf("%d", x);
+//		x += 1;
+//		}
+//
+//	printf("__\n\n");
+//	do
+//
+//	{
+//		x += 1;
+//		if (x == 5) break;
+//
+//		printf("%d", x);
+//
+//	} while (x < 11);
+//
+//	int a;
+//	int b;
+//	printf("insert first number\n");
+//	scanf("%d", &a);
+//	printf("insert second number\n");
+//	scanf("%d", &b);
+//
+//
+//	sum(a, b);
+//	
+//
+//
+//	system("pause");
+//
+//}
+//
+//int sum(int a, int b) {
+//	int i;
+//	int multi = 0;
+//	for (i = 0; i <= 100; i++)
+//	{
+//		multi += i;
+//	}
+//	printf("%d\n", multi);
+//	return multi;
+//	
+//}
+
+int main() {
+
+	printf("1.Book seats\n");
+
+	printf("2.buy seats\n");
+
+	printf("3.buy seats\n");
+
+	printf("5.buy seats\n");
+
+	printf("6.buy seats\n");
+
+	printf("7.buy seats\n");
+
+	printf("8.buy seats\n");
+
+
+		system("pause");
+}
